6a9ed9070b51c1a32fa0946f824d85a1c98109e9
gabriel-belo <rm551669@fiap.com.br>
2024-09-17 11:37:52 -0300

    terceiro commit

--- arquivo.py
+++ arquivo.py
@@ -4,6 +4,12 @@ import matplotlib.pyplot as plt
 posicao= []
 totalPontos=[]
 
+def grafico_piloto(nome, df):
+    index = df.query('Nome' == nome).index
+    #piloto = [df.loc[df['Nome'] == nome,"Mexico"], df.loc[df['Nome'] == nome,"Arabia Saudita 1"], df.loc[df['Nome'] == nome,"Arabia Saudita 2"]]
+    piloto = [df.at[index, 'Mexico'], df.at[index, 'Arabia Saudita 1'], df.at[index, 'Arabia Saudita 2'],]
+    print(piloto)
+
 infos={
     "Nome":["P. Wehrlein", "M. Evans", "N. Cassidy", "O. Rowland", "J. Vergne", "A. Felix da Costa", "J. Dennis",
             "M. Gunther", "R. Frinjs", "S. Vandoorne", "S. Buemi", "N. Muller", "S. Bird", "J. Hughes", "N. Nato", "E. Mortara" , "S. Fenestraz",
@@ -11,7 +17,7 @@ infos={
             "K. van der Linde", "J. King", "P. Aron", "C. Collet"],
     "Pais":["Alemanha", "Austrália", "Austrália", "Grã-Bretanha", "França", "Portugal", "Grã-Bretanha", "Alemanha", "Holanda", "Belgica", "Suiça",
             "Suiça", "Grã-Bretanha", "Grã-Bretanha", "França", "Suiça", "França", "Holanda", "Grã-Bretanha", "Brasil", "India", "Grã-Bretanha", "Brasil", "Suecia",
-            "Africa do Sul", "Grã-Bretanha", "Estonia", "Brasil",],
+            "Africa do Sul", "Grã-Bretanha", "Estonia", "Brasil"],
     "Mexico":[28, 10, 16, 0, 8, 0, 2, 12, 0, 4, 18, 0, 0, 6, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     "Arabia Saudita 1":[4, 10, 15, 0, 21, 0, 26, 6, 1, 0, 0, 0, 12, 0, 8, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
     "Arabia Saudita 2":[6, 1, 26, 18, 4, 0, 0, 2, 18, 10, 0, 0, 0, 12, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -36,6 +42,30 @@ dados_classificacao["Pontuação total"]= totalPontos
 
 print(pd.DataFrame(dados_classificacao))
 
-#ver pontuação de um piloto específico
-#
-readline
+#ver pontuação de um piloto específico com gráfico
+#ver pontuação das equipes
+#ver pontuação de todos os pilotos de uma equipe
+
+print("""
+Veja as informações em gráficos
+1- Escolha um piloto e veja sua pontuação em um gráfico
+2- Ver pontuação de uma equipe
+3-Ver pontuação de todos os pilotos de uma equipe""")
+
+opcao= int(input("Escolha uma das opções: "))
+
+match opcao:
+    case 1:
+        nome= input("Insira o nome do piloto: ")
+        grafico_piloto(nome, dados_classificacao)
+
+    case 2:
+        equipe= input("Insira o nome da equipe: ")
+        #grafico_equipe(equipe, dados_classificacao)
+
+    case 3:
+        equipe= input("Insira o nome da equipe: ")
+        #grafico_pilotos(equipe, dados_classificacao)
+
+    case _:
+        print("Opção inválida")
